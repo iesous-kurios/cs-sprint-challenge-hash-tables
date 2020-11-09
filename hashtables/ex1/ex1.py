6758bf3e7e9493bc11cfd1ef050a74010a500655
iesous-kurios <j.wylie.81@gmail.com>
2020-11-08 18:41:58 -0800

    passing all tests on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,28 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    It's in the name:
+    A Function to get the indices of item weights
+
+    Methodology:
+    I will take each item in the weights and subtract that from the limit
+    to get a difference. 
+    Then I will check to see if any of the items in weights, when added
+    to the difference, equal the limit.  Each time one is successfully
+    found I will append it.
+
+    Then, for proper format, I will take the higher of the two values 
+    and place them in position 0, as instructions asked me to. 
     """
-    # Your code here
-
-    # quick edit to open pull request 
-
-    return None
+    idx = []
+    lookup = {}
+    if length > 1:
+        for i in range(length):
+            lookup[weights[i]] = limit - weights[i]
+            if lookup[weights[i]] in weights:
+                idx.append(i)
+    
+    if len(idx) > 1:
+        if idx[0] > idx[1]:
+            return (idx[0], idx[1])
+        else:
+            return (idx[1], idx[0])
